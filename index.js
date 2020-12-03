eaa6326519aa8a16b75115d8dd0fe198413acbf6
mikohan <angara99@gmail.com>
2020-12-03 16:55:33 +0300

    Make own storecreateor

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const { createStore } = require('redux');
 
+function createStoreMy(rootReducer, initialState) {
+  let state = {};
+  let subscribers = [];
+  return {
+    dispatch(action) {
+      state = rootReducer(state, action);
+      subscribers.forEach((call) => call());
+    },
+    subscribe(callback) {
+      subscribers.push(callback);
+    },
+    getState() {
+      return state;
+    },
+  };
+}
+
 const MY_ACTION = 'MY_ACTION';
 
 const myAction = (payload) => {
@@ -27,10 +44,12 @@ const store = createStore(myReducer);
 
 console.log(store.getState(), 'Initial state expected');
 
-store.subscribe(() => {
+const unsubscribe = store.subscribe(() => {
   console.log('New state expected:', store.getState());
 });
 
 store.dispatch(myAction(67));
 store.dispatch(myAction(130));
 store.dispatch(myAction(499));
+
+unsubscribe();
