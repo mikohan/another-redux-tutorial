add363a89d684bda8d63716430a7dcee5534b629
mikohan <angara99@gmail.com>
2020-12-03 14:36:25 +0300

    Created store

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('index js');
+const { createStore } = require('redux');
+
+const MY_ACTION = 'MY_ACTION';
+
+const myAction = (payload) => {
+  return {
+    type: MY_ACTION,
+    payload: payload,
+  };
+};
+
+const initialState = {
+  myProperty: {
+    one: 34,
+  },
+};
+
+function myReducer(state = initialState, action) {
+  switch (action.type) {
+    case MY_ACTION:
+      return {
+        ...state,
+        myProperty: {
+          one: action.payload,
+        },
+      };
+    default:
+      return { state };
+  }
+}
+
+const store = createStore(myReducer);
+
+store.dispatch(myAction(67));
+
+console.log(store.getState());
